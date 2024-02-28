a0f6a297a81f39b465b4b23c5f297338650c976f
vdaita <vdaita@gmail.com>
2024-02-28 03:00:14 -0600

    Short JSON streams in quick succession no longer throw errors on the frontend parser

--- main.py
+++ main.py
@@ -56,15 +56,24 @@ def generate_multifile_response(directory, objective, snippets):
 
 def generate_response(directory, objective, snippets, verbose=True, use_vectorstore=True, user_input=False):
         global codebase_retriever
-        yield json.dumps({"information": "Started processing information!"})
+        yield json.dumps({
+            "type": "information",
+            "content": "Started processing information!"
+        }) + "<sddlm>"
     
         # Perform information retrieval
         if not(codebase_retriever.directory) == directory:
             codebase_retriever = CodebaseRetriever(directory, use_vectorstore=use_vectorstore)
             codebase_retriever.load_all_documents()
-            yield json.dumps({"information": "Loading vectorstore search for your codebase."})
+            yield json.dumps({
+                "type": "information",
+                "content": "Loading vectorstore search for your codebase."
+                }) + "<sddlm>"
         else:
-            yield json.dumps({"information": "Existing codebase retriever works."})
+            yield json.dumps({
+                "type": "information",
+                "content": "Existing codebase retriever works."
+                }) + "<sddlm>"
 
         start_time = time.time()
         model_request = information_request_model(INFORMATION_RETRIEVAL_PROMPT,[f"Objective: {objective} \n \n Existing information {snippets}"])
@@ -74,47 +83,57 @@ def generate_response(directory, objective, snippets, verbose=True, use_vectorst
             print("Performed Information Retrieval Request: ", (end_time - start_time))
 
         information = snippets
+
+        SPLIT_TOKEN = "------"
         
-        # start_time = time.time()
-        # for _ in range(1): # Setting a limit to 3 queries at the most. TODO: find a fix to the recursive problem eh
-        #     # With the queries, run the corresponding searches
-        #     external_queries = extract_xml_tags(model_request, "a")
-        #     internal_queries = extract_xml_tags(model_request, "b")
-        #     # file_read_queries = extract_xml_tags(model_request, "f")
+        start_time = time.time()
+        for _ in range(1): # Setting a limit to 3 queries at the most. TODO: find a fix to the recursive problem eh
+            # With the queries, run the corresponding searches
+            external_queries = extract_xml_tags(model_request, "a")
+            internal_queries = extract_xml_tags(model_request, "b")
+            # file_read_queries = extract_xml_tags(model_request, "f")
 
-        #     if verbose:
-        #         print("Processing information retrieval request with: ", model_request)
-        #         print(" External queries: ", json.dumps(external_queries, indent=4))
-        #         print(" Internal queries: ", json.dumps(internal_queries, indent=4))
-        #         # print(" File read queries: ", json.dumps(file_read_queries, indent=4))
+            if len(external_queries) > 4:
+                external_queries = external_queries[:4]
+            if len(internal_queries) > 4:
+                internal_queries = internal_queries[:4]
 
-        #     for query in external_queries:
-        #         if verbose:
-        #             print("Processing external query: ", query)
-        #         external_response = external_retriever.answer_question(query)
-        #         if verbose:
-        #             print("Received response: ", external_response)
-        #         information += f"\n# External Query: {query} \n {external_response}"
+            if verbose:
+                print("Processing information retrieval request with: ", model_request)
+                print(" External queries: ", json.dumps(external_queries, indent=4))
+                print(" Internal queries: ", json.dumps(internal_queries, indent=4))
+                # print(" File read queries: ", json.dumps(file_read_queries, indent=4))
 
-        #     for query in internal_queries:
-        #         code_snippets = codebase_retriever.retrieve_documents(query, None)
-        #         code_snippets_string = "\n".join(code_snippets)
-        #         information += f"\n# Codebase Query: {query} \n {code_snippets_string}"
+            for query in external_queries:
+                if verbose:
+                    print("Processing external query: ", query)
+                external_response = external_retriever.answer_question(query)
+                if verbose:
+                    print("Received response: ", external_response)
+                information += f"{SPLIT_TOKEN}\n# External Query: {query} \n {external_response}"
+
+            for query in internal_queries:
+                code_snippets = codebase_retriever.retrieve_documents(query, None)
+                code_snippets_string = "\n".join(code_snippets)
+                information += f"{SPLIT_TOKEN}\n# Codebase Query: {query} \n {code_snippets_string}"
             
-        #     # for query in file_read_queries:
-        #     #     accurate_file = find_closest_file(directory, query)
-        #     #     contents = open(accurate_file, "r").read()
-        #     #     information += f"\n# File Contents for: {query} \n ```\n{contents}\n```"
+            # for query in file_read_queries:
+            #     accurate_file = find_closest_file(directory, query)
+            #     contents = open(accurate_file, "r").read()
+            #     information += f"\n# File Contents for: {query} \n ```\n{contents}\n```"
 
-        #     if len(external_queries) == 0 and len(internal_queries) == 0: # and len(file_read_queries) == 0:
-        #         break
-        # end_time = time.time()
+            if len(external_queries) == 0 and len(internal_queries) == 0: # and len(file_read_queries) == 0:
+                break
+        end_time = time.time()
 
         if verbose:
-            print("Performed all information retrieval requests: ", (start_time - end_time))
+            print("Performed all information retrieval requests: ", (end_time - start_time))
 
         # Send back the documents and ask for feedback
-        yield json.dumps({"All information": information}, indent=4)
+        yield json.dumps({
+            "type": "context",
+            "content": information
+        }, indent=4) + "<sddlm>"
         
         if user_input:
             new_information = wait_for_gui_response(time.time())
@@ -135,7 +154,10 @@ def generate_response(directory, objective, snippets, verbose=True, use_vectorst
             print("Generated plan in: ", (start_time - end_time))
 
         # Get feedback
-        yield json.dumps({"Feedback: ": plan})
+        yield json.dumps({
+            "type": "plan",
+            "content": plan
+        }, indent=4) + "<sddlm>"
 
         if user_input:
             plan = wait_for_gui_response(time.time())
@@ -158,13 +180,13 @@ def generate_response(directory, objective, snippets, verbose=True, use_vectorst
         #     print("Finishing refinement in time: ", end_time - start_time)
 
         # Return a set of changes to the user
-        yield json.dumps({"changes": changes})
+        yield json.dumps({"changes": changes}) + "<sddlm>"
 
 @app.post("/process")
 def ask():
     data = request.get_json()
     directory, objective, snippets = data["directory"], data["objective"], data["snippets"]
-    return app.response_class(stream_with_context(generate_response(directory, objective, snippets)))
+    return app.response_class(stream_with_context(generate_response(directory, objective, snippets, user_input=True)))
 
 @app.post("/send_response")
 def send_response():
@@ -177,34 +199,38 @@ def send_response():
 def wait_for_gui_response(request_time):
     global response, response_time
     while request_time < response_time:
+        print("     No response received: waiting for GUI response")
         time.sleep(0.5)
     return response
-    
+
 if __name__ == "__main__":
-    directory = "/Users/vijaydaita/Files/uiuc/rxassist/rxmind-nextjs-main"
-    filepath = "/app/pages/quiz/page.tsx"
+    app.run(port=8125, debug=True)
+    
+# if __name__ == "__main__":
+#     directory = "/Users/vijaydaita/Files/uiuc/rxassist/rxmind-nextjs-main"
+#     filepath = "/app/pages/quiz/page.tsx"
 
-    read_file = open(directory + filepath, "r")
-    file_snippet = read_file.read()
-    read_file.close()
+#     read_file = open(directory + filepath, "r")
+#     file_snippet = read_file.read()
+#     read_file.close()
 
-    for value in generate_response(
-            directory, 
-            "In the main quiz page, add a modal for when the quiz is over that shows the score and allows you to retake the quiz.", 
-            f"Filepath: {filepath} \n ```\n{file_snippet}\n```",
-            use_vectorstore=False
-        ):
-        print(json.dumps(json.loads(value), indent=4))
-        ret_object = json.loads(value)
-        if "changes" in ret_object:
-            for filepath in ret_object["changes"]: # For each file
-                print(f"# Filepath: {filepath}")
-                print(ret_object["changes"][filepath])
+#     for value in generate_response(
+#             directory, 
+#             "In the main quiz page, add a modal for when the quiz is over that shows the score and allows you to retake the quiz.", 
+#             f"Filepath: {filepath} \n ```\n{file_snippet}\n```",
+#             use_vectorstore=False
+#         ):
+#         print(json.dumps(json.loads(value), indent=4))
+#         ret_object = json.loads(value)
+#         if "changes" in ret_object:
+#             for filepath in ret_object["changes"]: # For each file
+#                 print(f"# Filepath: {filepath}")
+#                 print(ret_object["changes"][filepath])
 
-            should_apply = input("Apply to rewrite? Send Y for yes. ")
-            if should_apply.lower() == "y":
-                for filepath in ret_object["changes"]:
-                    file = open(os.path.join(directory, filepath), "w")
-                    file.write(ret_object["changes"][filepath])
-                    file.close()
+#             should_apply = input("Apply to rewrite? Send Y for yes. ")
+#             if should_apply.lower() == "y":
+#                 for filepath in ret_object["changes"]:
+#                     file = open(os.path.join(directory, filepath), "w")
+#                     file.write(ret_object["changes"][filepath])
+#                     file.close()
     # app.run(port=8123, debug=True)
